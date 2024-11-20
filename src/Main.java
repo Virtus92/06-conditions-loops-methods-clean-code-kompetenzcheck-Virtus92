75d2b64d68219d0baa82bbefc8cba974df0d80aa
Dinel <dinel92x@gmail.com>
2024-11-20 13:25:59 +0100

    v1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        char[] assigned = {'@', '8', '(', 'D', '3', 'F', '6', '#', '!', 'J', 'K', '1', 'M', 'N', '0', 'P', 'Q', 'R', '$', '7', 'U', 'V', 'W', 'X', 'Y', '2'};
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Gib mir einen Text den ich übersetzen soll.");
+        String sentence = sc.nextLine();
+        sentence = sentence.toUpperCase();
+
+        char[] converted = sentence.toCharArray();
+
+        for (int i=0; i<converted.length; i++) {
+            char c = converted[i];
+            for (int j=0; j<alphabet.length; j++) {
+                if (converted[i] == ' ') {
+                    converted[i] = ' ';
+                    System.out.print(converted[i]);
+                    break;
+                } else if (c == alphabet[j]) {
+                    converted[i] = assigned[j];
+                    System.out.print(converted[i]);
+                }
+            }
+        }
     }
 }
